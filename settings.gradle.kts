805fa155fdeb9d0b5b24ede2a95205baafa9ccfd
jaxon93 <jaxon932002@gmail.com>
2023-11-24 03:42:50 +0900

    added

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android_telegram_ui"
+include(":app")
+include(":telegram_contacts_kotlin")
+include(":telegram_notifications_kotlin")
